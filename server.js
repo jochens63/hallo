d9457078247bea5b81f65577f5eb1448149df582
Jochen Schneider <jochen.schneider@de.ibm.com>
2020-09-13 09:52:13 +0200

    2nd

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require("express"),
     app = express();
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 8082;
 
 app.use(express.static(__dirname + '/public'));
 
